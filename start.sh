b3eedd385c3d5b1f96e490ddc8d851645d60a5c5
romap0 <pavlov@pqlab.ru>
2020-04-17 18:31:19 +0900

    Fix start script

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:`pwd`";
+export LD_PRELOAD=`pwd`/libruntime.so;
 
 # clang -std=c99 -Wall -pedantic -o pa2 -g *.c libruntime.dylib
-gcc -std=c99 -Wall -pedantic -o pa2 -g *.c -L. -lruntime
-
-LD_PRELOAD=`pwd`/libruntime.so ./pa2 $* && rm -rf pa2 pa2.dSYM
+gcc -std=c99 -Wall -Werror -pedantic -o pa2 -g *.c -L. -lruntime && ./pa2 $* && rm -rf pa2 pa2.dSYM

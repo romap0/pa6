5c896d5ea0101a4cac0812b1506561b50940734e
romap0 <pavlov@pqlab.ru>
2020-04-20 00:50:33 +0900

    Rename

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:`pwd`";
 export LD_PRELOAD=`pwd`/libruntime.so;
 
-# clang -std=c99 -Wall -pedantic -o pa4 -g *.c libruntime.dylib
-# gcc -std=c99 -Wall -Werror -pedantic -o pa4 -g *.c -L. -lruntime && ./pa4 $* && rm -rf pa4 pa4.dSYM
-gcc -std=c99 -Wall -Werror -pedantic -o pa4 -g *.c -L. -lruntime && ./pa4 -p 3 10 20 30 --mutexl && rm -rf pa4 pa4.dSYM
+# clang -std=c99 -Wall -pedantic -o pa6 -g *.c libruntime.dylib
+# gcc -std=c99 -Wall -Werror -pedantic -o pa6 -g *.c -L. -lruntime && ./pa6 $* && rm -rf pa6 pa6.dSYM
+gcc -std=c99 -Wall -Werror -pedantic -o pa6 -g *.c -L. -lruntime && ./pa6 -p 3 10 20 30 --mutexl && rm -rf pa6 pa6.dSYM
